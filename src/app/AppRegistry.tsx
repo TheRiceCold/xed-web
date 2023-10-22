a9fcc0d01ea4492f2f04e38836e9ce9bea895047
Dale Walter <dalewaltergh@gmail.com>
2023-10-22 14:20:44 +0800

    Setup Tailwind

--- src/app/AppRegistry.tsx
+++ src/app/AppRegistry.tsx
@@ -3,10 +3,6 @@
 import { Amplify } from 'aws-amplify'
 import awsConfig from '@/aws-exports'
 
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-
-import theme from '@/app/theme'
 import { AuthContext } from '@/context'
 
 Amplify.configure({ ...awsConfig, ssr: true })
@@ -17,10 +13,7 @@ const AppRegistry = ({
   children: React.ReactNode
 }) => (
   <AuthContext>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    {children}
   </AuthContext>
 )
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import AppRegistry from './AppRegistry'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Xed',
