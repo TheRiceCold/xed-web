125c1cdb86bd084ef5c5098d2abe9c495f0c6bd5
Dale Walter <dalewaltergh@gmail.com>
2023-11-02 05:28:55 +0800

    Move form components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,2 @@
-// export { default as Form } from './Form'
-// export { default as TextInput } from './TextInput'
-// export { default as PasswordInput } from './PasswordInput'
-
 export { default as GetAppDialog } from './GetAppDialog'
 export { default as AuthDialog } from './auth/AuthDialog'
