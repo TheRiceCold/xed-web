f58a3f84782828ccfe1bd26d4ad2ad009624594d
Dale Walter <dalewaltergh@gmail.com>
2023-11-11 12:10:21 +0800

    Remove previous chat components

--- src/components/chat/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Body } from './Body'
-export { default as Form } from './Form'
-export { default as Header } from './Header'
-export { default as EmptyState } from './EmptyState'
